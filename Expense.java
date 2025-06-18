94fbead22ff695080381210c91522dc748921365
harshal-john <harshaljohnv@gmail.com>
2025-06-18 13:37:24 +0530

    Override toString() to print the values

--- Expense.java
+++ Expense.java
@@ -1,19 +1,25 @@
 import java.time.LocalDate;
 
 public class Expense {
-    private String catagory;
+    private String category;
     private double amount;
     private LocalDate date;
 
     // Public Init Method 
-    public Expense(String catagory, double amount, LocalDate date){
-        this.catagory = catagory;
+    public Expense(String category, double amount, LocalDate date){
+        this.category = category;
         this.amount = amount;
         this.date = date;
     }
 
     // Methods to invoke items
-    public String getCatagory() { return catagory;}
-    public String getamount() { return catagory;}
-    public String getdate() { return catagory;}
+    public String getcategory() { return category;}
+    public double getamount() { return amount;}
+    public LocalDate getdate() { return date;}
+
+    // Override toString() to print the values 
+    @Override
+    public String toString(){
+        return date + " | " + category + " | ₹" + amount;
+    }
 }
