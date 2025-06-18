8fd5aacf45f873b27993dd7512d7a68dd314ad04
Harshal John V <harshaljohnv@gmail.com>
2025-06-18 13:08:54 +0530

    expense_init_method

--- Expense.java
+++ Expense.java
@@ -4,4 +4,11 @@ public class Expense {
     private String catagory;
     private double amount;
     private LocalDate date;
+
+    // Public Init Method 
+    public Expense(String catagory, double amount, LocalDate date){
+        this.catagory = catagory;
+        this.amount = amount;
+        this.date = date;
+    }
 }
