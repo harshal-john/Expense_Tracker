73bd37a7ba9f0bc19255320e4d424481db723a8b
harshal-john <harshaljohnv@gmail.com>
2025-06-18 20:00:55 +0530

    creating main method and objects for class

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class ExpenseTracker {
-    
+    public static void main(String[] args) {
+        // Creating scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Create object for Expense Manager
+        ExpenseManager manager = new ExpenseManager();
+
+        try{
+            manager.loadFormFile("expense.txt");
+        } catch(Exception e) {
+            System.out.println("No previous data found Exception : " + e);
+        }
+    }
 }
